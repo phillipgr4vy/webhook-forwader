76c22ee18ecea9c4cd68dc74762bdcac24d9c3f0
Phillip Thomas <phillip@gr4vy.com>
2025-05-29 14:07:17 -0400

    fix build

--- src/webhook_forwarder/config.py
+++ src/webhook_forwarder/config.py
@@ -1,7 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 OAUTH_TOKEN_URL = os.getenv("OAUTH_TOKEN_URL")
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -9,3 +6,8 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 FORWARD_URL = os.getenv("FORWARD_URL")
+
+# Optional: Raise if required vars are missing
+required_vars = [OAUTH_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD, FORWARD_URL]
+if any(var is None for var in required_vars):
+    raise RuntimeError("Missing one or more required environment variables.")
